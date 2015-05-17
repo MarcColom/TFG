43608914ba345e5c813c78146147065916ccde85
Marc Colom <mcolomro@gmail.com>
2015-05-17 23:49:56 +0200

    ok - fins order

--- TFG/docroot/AngularJS/app.js
+++ TFG/docroot/AngularJS/app.js
@@ -5,14 +5,19 @@
         	
         	$routeProvider
         	.when("/",{
-        		controller: "InsuranceRequestCtrl",
-        		controllerAs: "vm",
-        		templateUrl: "findInsuranceView.html"
+        		controller: "UserFormCtrl",
+        		controllerAs: "formCtrl",
+        		templateUrl: "findInsuranceView.html"       			
         	})
         	.when("/showInsurancesView",{
-        		controller: "InsuranceRequestCtrl",
-        		controllerAs: "vm",
-        		templateUrl: "showInsurancesView.html"
+        		controller: "ShowInsuranceCtrl",
+        		controllerAs: "showCtrl",
+        		templateUrl: "showInsurancesView.html"        		
+        	})
+        	.when("/orderView",{
+        		controller: "OrderCtrl",
+        		controllerAs: "orderCtrl",
+        		templateUrl: "orderView.html"        		
         	})
         	.when("/contact",{
         		controller: "contactCtrl",
@@ -23,42 +28,92 @@
         		controller: "contactCtrl",
         		controllerAs: "ct",
         		templateUrl: "faqs.html"
-        	})
-        	
-        	
-        	.otherwise({ reditrectTo : "/" });
-        	
+        	})        	
+        	.otherwise({ reditrectTo : "/" });        	
         	
         })
+        
+        
+        .factory("formFactory", function(){        	 
+        	
+        	var data = {};            
+            data.today = new Date(),
+            data.form = {};          
+                         
+            return data;
+        })  
+        
+        .factory("findInsuranceFactory", function(){
+        	       	
+        	var data = this;
+        	data.form = {};
+        	data.insurances = {};       	      	
+        	        	
+        	return data;     	        	     	
+        })
+        
+        .factory("orderFactory", function(){
+        	       	
+        	var data = {};
+        	data.form = {};
+        	data.insurance = {};
+        	data.user = {};
+        	        	
+        	return data;     	        	     	
+        })
+        
                 	
-        .controller('InsuranceRequestCtrl', ['$http', function($http){		 
-		    
-        		var vm=this;
-        		vm.insurances = {};
-        		vm.users = {};        		
-        		vm.today = new Date();        		
-		        
-		        vm.AddRestEJB = function(){   	        	        	
-		        	
-		        	  $http.get("http://localhost:8080/SegurosyViajes/WSUserInsuranceRest/findInsurances", {params: {origin: vm.user.origin,
-		        		  																							destination: vm.user.destination,
-		        		  																							initDate: vm.user.initDate,
-		        		  																							endDate: vm.user.endDate}
-		        	  																						})	        	
-		        		.success(function(respuesta){                
-		        			vm.insurances = respuesta;		 
-		        			console.log("OK DE POST");
-		        		})
-		        		.error(function(){	        				 
-		        			console.log("ERROR DE POST");
-		        		});
-		        	
-		        	//vm.insurances.push(vm.order);
-		        	//vm. order = {};
-		        	vm.user = {};		        	
-		        }  
-		      
-         }])
+        .controller('UserFormCtrl', function($location, formFactory, findInsuranceFactory){		 
+		
+        		var vm = this;        		
+        		vm.today = formFactory.today;        		     		
+        		  		
+        		vm.AddForm = function(){	        
+        			formFactory.form = vm.user;		        	
+		        	vm.user = {};		       	        	
+		        	$location.url("/showInsurancesView");	        	
+		        }		      
+         })
+         
+         .controller('ShowInsuranceCtrl', function($http, $location, formFactory, findInsuranceFactory, orderFactory){	
+        	
+        	 var vm = this;       	 
+        	 vm.insurances = {};
+        	 vm.form = formFactory.form;        	 
+        	 
+        	 $http.get("http://localhost:8080/SegurosyViajes/WSUserInsuranceRest/findInsurances", {params: 
+ 				{origin: vm.form.origin,
+				destination: vm.form.destination,
+				initDate: vm.form.initDate,
+				endDate: vm.form.endDate}
+        	 })	        	
+			.success(function(respuesta){				
+				vm.insurances = respuesta;								
+				console.log("OK DE POST");
+			})
+			.error(function(){	        				 
+				console.log("ERROR DE POST");
+			});  
+        	 
+        	vm.AddInsurance = function(insurance){
+        		vm.myInsurance = insurance;
+        		orderFactory.form = vm.form;
+        		orderFactory.insurance = vm.myInsurance;        		
+        		$location.url("/orderView");	
+        		
+        	}
+        	 
+         })
+         
+         .controller('OrderCtrl', function($location, orderFactory){		 
+		
+        		var vm = this;        		
+        		vm.insurance = orderFactory.insurance;
+        		vm.form = orderFactory.form;	  		
+        	
+         })
+	 		
+         
          
 	 	.controller('contactCtrl', ['$http', function($http){		 
 	 		
